2999bce30fa31237a0a04d7927ed849ee287a3a7
jhlee401 <jh.lee4@allenslibrary.com>
2026-04-10 11:12:59 +0900

    feat: DataLoader load 구현

--- src/DataLoader.ts
+++ src/DataLoader.ts
@@ -1,14 +1,30 @@
 type BatchFn<K, V> = (keys: readonly K[]) => Promise<(V | Error)[]>;
+type Batch<K, V> = {
+  keys: Array<K>,
+  callbacks: Array<{
+    resolve: (value: V) => void;
+    reject: (error: Error) => void,
+  }>
+}
 
 export class DataLoader<K, V> {
   private batchFn: BatchFn<K, V>;
+  private batch: Batch<K,V> | null;
 
   constructor(batchFn: BatchFn<K, V>) {
     this.batchFn = batchFn;
+    this.batch = null;
   }
 
   load(key: K): Promise<V> {
-    throw new Error('Not implemented');
+    const batch = this.getCurrentBatch();
+
+    batch.keys.push(key);
+    const promise = new Promise<V>((resolve, reject) => {
+      batch.callbacks.push({resolve, reject});
+    })
+
+    return promise;
   }
 
   clear(key: K): this {
@@ -17,5 +33,38 @@ export class DataLoader<K, V> {
 
   clearAll(): this {
     throw new Error('Not implemented');
+  }
+
+  private getCurrentBatch(): Batch<K,V> {
+    if(this.batch !== null) {
+      return this.batch;
+    }
+
+    const newBatch: Batch<K,V> = {
+      keys: [],
+      callbacks: []
+    };
+    this.batch = newBatch;
+
+    process.nextTick(() => this.dispatchBatch(newBatch));
+
+    return newBatch;
+  }
+
+  private async dispatchBatch(batch: Batch<K, V>): Promise<void> {
+    this.batch = null;
+    try {
+      const values = await this.batchFn(batch.keys);
+      batch.callbacks.forEach((callback, i) => {
+        const value = values[i];
+        if (value instanceof Error) {
+          callback.reject(value);
+        } else {
+          callback.resolve(value);
+        }
+      });
+    } catch (error) {
+      batch.callbacks.forEach((callback) => callback.reject(error as Error));
+    }
   }
 }
